a563de9b65d52519357fa65a7b2e6e2a82e5418c
Stanisław Zaprzalski <zaprzalskistanislaw@gmail.com>
2024-03-24 21:25:01 +0100

    finale

--- ObjectOrientedDesign/UpdateFunctions.cs
+++ ObjectOrientedDesign/UpdateFunctions.cs
@@ -22,16 +22,29 @@ namespace ObjectOrientedDesign
             {
                 landing.AddDays(1);
                 landingticks = landing.Ticks;
-            }        
+            }
             if (takeoffticks < nowticks && landingticks > nowticks)
             {
                 float l = (float)(nowticks - takeoffticks) / (landingticks - takeoffticks);
                 float k = (float)(nowticks - takeoffticks) / (landingticks - takeoffticks);
-                float NewLatitude = origin.Latitude + l * (dest.Latitude - origin.Latitude);
-                float NewLongitude = origin.Longitude + k * (dest.Longitude - origin.Longitude);
+                //    float NewLatitude = origin.Latitude + l * (dest.Latitude - origin.Latitude);
+                //    float NewLongitude = origin.Longitude + k * (dest.Longitude - origin.Longitude);
+                float NewLatitude = Single.Lerp(origin.Latitude, dest.Latitude, l);
+                float NewLongitude = Single.Lerp(origin.Longitude, dest.Longitude, k);
                 double degrees;
-                degrees = Math.Atan2(dest.Longitude - origin.Longitude, dest.Latitude - origin.Latitude);
-
+                if (fl.Latitude == int.MaxValue)
+                { 
+                    degrees = Math.Atan2(dest.Longitude - origin.Longitude, dest.Latitude - origin.Latitude);
+                }
+                else
+                {
+                    var pos1 = SphericalMercator.FromLonLat(fl.Longitude, fl.Latitude);
+                    var pos2 = SphericalMercator.FromLonLat(NewLongitude, NewLatitude);
+                    fl.Latitude = NewLatitude;
+                    fl.Longitude = NewLongitude;
+                    degrees = Math.Atan2(pos2.x - pos1.x, pos2.y - pos1.y);
+                }
+                
                 list.Add(new FlightAdapter(fl, NewLatitude, NewLongitude, degrees));
             }
         }
